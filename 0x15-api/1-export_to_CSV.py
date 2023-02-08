eb7c39cebab73ce901fbdca1886e41d626147d35
wac <wabaham9@gmail.com>
2023-02-08 20:25:33 +0000

    0x15/second

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-""" show employee todo list progress """
+""" show employee todo list progress and export to csv"""
 
 if __name__ == "__main__":
     from sys import argv
+    import csv
     import json
     import urllib.request
 
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     d_todos = json.loads(todos.read())
     t_title = []
     t_count = 0
+    t_owned = []
     done_count = 0
     for td in d_todos:
         if td["userId"] == idd:
@@ -23,8 +25,13 @@ if __name__ == "__main__":
                 done_count += 1
                 t_title.append(td["title"])
             t_count += 1
-
+            det = [idd, d_emp["username"], td["completed"], td["title"]]
+            t_owned.append(det)
     print("Employee {} is done with tasks({}/{}):".format(d_emp["name"],
           done_count, t_count))
     for i in t_title:
         print("\t {}".format(i))
+
+    with open("{}.csv".format(idd), 'wt') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        csv_writer.writerows(t_owned)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-""" show employee todo list progress """
+""" show employee todo list progress and export to JSON """
 
 if __name__ == "__main__":
     from sys import argv
-    import json
     import urllib.request
+    import json
 
     idd = int(argv[1])
     emp_url = "https://jsonplaceholder.typicode.com/users/" + str(idd)
@@ -16,6 +16,8 @@ if __name__ == "__main__":
     d_todos = json.loads(todos.read())
     t_title = []
     t_count = 0
+    t_owned = []
+    det = []
     done_count = 0
     for td in d_todos:
         if td["userId"] == idd:
@@ -23,8 +25,14 @@ if __name__ == "__main__":
                 done_count += 1
                 t_title.append(td["title"])
             t_count += 1
-
+            tsks = {"task": td["title"], "completed": td["completed"],
+                    "username": d_emp["username"]}
+            det.append(tsks)
+    t_owned = {idd: det}
     print("Employee {} is done with tasks({}/{}):".format(d_emp["name"],
           done_count, t_count))
     for i in t_title:
         print("\t {}".format(i))
+
+    with open("{}.json".format(idd), 'w') as f:
+        json.dump(t_owned, f)
