b156b9c8d48b60f5087e9dae4fc4f8df179fee32
wac <wabaham9@gmail.com>
2023-02-28 18:33:40 +0000

    0x16/co

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ shows the number of subscribers a subredit """
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    res = requests.get(url)
-    d = json.loads(res.text)
-    subs = d['data']['children'][1]['data']['subreddit_subscribers']
-    return int(subs)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
