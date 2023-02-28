021d9504cbd4d084cf0900441e3ab49ba51bcf04
wac <wabaham9@gmail.com>
2023-02-28 14:37:42 +0000

    0x16/first

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fetch the nuber of subscribers of a subreddit from reddit """
+from urllib import request
+import json
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """ shows the number of subscribers a subredit """
+    param = argv[1]
+    url = 'https://www.reddit.com/r/{}.json'.format(param)
+    with request.urlopen(url) as res:
+        if res.getcode() == 200:
+            data = res.read()
+            d = json.loads(data)
+            subs = d['data']['children'][1]['data']['subreddit_subscribers']
+            return int(subs)
