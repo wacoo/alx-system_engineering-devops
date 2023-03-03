e347eb2de2db9e5d35628c8c07fcb352b2c33e4c
wac <wabaham9@gmail.com>
2023-03-03 10:37:11 +0000

    second commit

---
+++ 0x17-web_stack_debugging_3/0-trace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
